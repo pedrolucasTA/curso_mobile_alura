dc03665187e29af4fcd9ffcd96ec8f3094bd63cb
Pedro Lucas <096823@aluno.uricer.edu.br>
2021-01-06 19:48:19 -0300

    primeiro commit

---
+++ app/src/main/java/alura/com/agenda/ui/ListaAlunosView.java
@@ -0,0 +1,52 @@
+package alura.com.agenda.ui;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import alura.com.agenda.dao.AlunoDAO;
+import alura.com.agenda.model.Aluno;
+import alura.com.agenda.ui.adapter.ListaAlunosAdapter;
+
+public class ListaAlunosView {
+
+    private final ListaAlunosAdapter adapter;
+    private final AlunoDAO dao;
+    private final Context context;
+
+    public ListaAlunosView(Context context) {
+        this.context = context;
+        this.adapter = new ListaAlunosAdapter(this.context);
+        dao = new AlunoDAO();
+    }
+
+    public void confirmaRemocao(final MenuItem item) {
+        new AlertDialog
+                .Builder(context)
+                .setTitle("Removendo Aluno")
+                .setMessage("Tem certeza que quer remover o aluno?")
+                .setPositiveButton("Sim", (dialog, which) -> {
+                    AdapterView.AdapterContextMenuInfo menuInfo =
+                            (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+                    Aluno alunoEscolhido = adapter.getItem(menuInfo.position);
+                    remove(alunoEscolhido);
+                })
+                .setNegativeButton("Não", null)
+                .show();
+    }
+
+    public void atualizaAlunos() {
+        adapter.atualiza(dao.todos());
+    }
+
+    private void remove(Aluno aluno) {
+        dao.remove(aluno);
+        adapter.remove(aluno);
+    }
+
+    public void configuraAdapter(ListView listaDeAlunos) {
+        listaDeAlunos.setAdapter(adapter);
+    }
+}
---
+++ app/src/main/java/alura/com/agenda/ui/activity/ConstantesActivities.java
@@ -0,0 +1,5 @@
+package alura.com.agenda.ui.activity;
+
+public interface ConstantesActivities {
+    String CHAVE_ALUNO = "aluno";
+}
